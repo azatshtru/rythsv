6dde6f2a076223f8c6b9a875657e75bf15abef3e
azatshtru <adeveloper121@gmail.com>
2026-08-20 22:30:48 +0530

    bugfix: underscore and dunder inline resolution

--- src/lib/editor/rysvmdInlineParser.ts
+++ src/lib/editor/rysvmdInlineParser.ts
@@ -24,7 +24,6 @@ export function rysvmdInlineParser() {
                         underscoreRun += 1;
                         current += 1;
                     }
-                    console.log(underscoreRun);
                     switch(underscoreRun) {
                         case 1: return cx.addDelimiter(underscore, pos, pos + 1, true, true);
                         case 2: return cx.addDelimiter(dunder, pos, pos + 2, true, true);
