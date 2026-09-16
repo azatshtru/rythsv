7abbe1800e157e476e1b484299489930da44e91e
azatshtru <adeveloper121@gmail.com>
2026-09-17 00:06:11 +0530

    refactor: removed unused utility classes

--- src/lib/editor/rysvmdInlineMathWidget.ts
+++ src/lib/editor/rysvmdInlineMathWidget.ts
@@ -10,14 +10,7 @@ export class InlineMathWidget extends WidgetType {
     toDOM() {
         const span = document.createElement('span');
         span.classList.add(
-            'z-10',
-            'inline-block',
             'align-baseline',
-            'h-fit',
-            'rounded-lg',
-            'border-slate-100',
-            'bg-white',
-            'text-black',
             'pointer-events-none',
             'select-none',
             'drag-none',
